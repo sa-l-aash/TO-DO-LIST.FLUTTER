3017ba9a5e207d091f45844a34e77f818a1b07a4
sa-l-aash <salaashkaelo2gmail.com>
2023-07-20 14:32:42 +0300

    built the tasks page

--- app/lib/screens/tasks_page.dart
+++ app/lib/screens/tasks_page.dart
@@ -1,8 +1,11 @@
+import 'package:app/screens/completed.dart';
+import 'package:app/screens/favorites.dart';
+import 'package:app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:app/models/task.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' as path;
-// import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
@@ -43,14 +46,158 @@ class _TasksPageState extends State<TasksPage> {
     });
   }
 
+  void saveTaskToDB(Task task) async {
+    //this method saves tasks to the database
+    final database = await openDatabase(
+      path.join(await getDatabasesPath(), 'todo.db'),
+      onCreate: (db, version) => {
+        db.execute(
+            'CREATE TABLE tasks(id INTEGER PRIMARY KEY , title TEXT, description VARCHAR , isDone INTEGER)')
+      },
+      version: 1,
+    );
+    await database.insert('tasks', task.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+    //works the same way as the snackbar
+    Fluttertoast.showToast(msg: 'Task Added Successfully');
+  }
+
+  void showAddTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        Null id;
+        String title = '';
+        String description = '';
+
+        //this displays above another page
+        return AlertDialog(
+            title: const Text('Add Task'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  onChanged: (value) => title = value,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                TextField(
+                  onChanged: (value) => description = value,
+                  decoration: const InputDecoration(labelText: 'Description'),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  final newTask =
+                      Task(id: 2, title: title, description: description);
+                  setState(() {
+                    tasks.add(newTask);
+                    Navigator.of(context).pop();
+                  });
+                },
+                child: const Text('Save '),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop,
+                child: const Text('Cancel'),
+              )
+            ]);
+      },
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+        //this moves the textfield away from the edge of the screen
+        padding: const EdgeInsets.all(10),
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          );
+          final task = tasks[index];
+          return Card(
+            child: ListTile(
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite),
+              ),
+              tileColor: const Color.fromARGB(255, 105, 7, 7),
+              title: Text(task.title),
+              subtitle: Text(task.description),
+              //this makes the textfields bigger or smaller
+              dense: false,
+              trailing: Checkbox(
+                  value: task.isDone,
+                  onChanged: (value) {
+                    setState(() {
+                      tasks[index].isDone = value!;
+                    });
+                  }),
+            ),
+          );
+        });
+  }
+
+//this is the ui
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Your Tasks'),
-          backgroundColor: const Color.fromARGB(255, 105, 7, 7),
-          automaticallyImplyLeading: false,
+      appBar: AppBar(
+        title: const Text('Your Tasks'),
+        backgroundColor: const Color.fromARGB(255, 105, 7, 7),
+        automaticallyImplyLeading: false,
+      ),
+      body: _buildList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: showAddTaskDialog,
+        child: const Icon(
+          Icons.add,
         ),
-        body: const SingleChildScrollView());
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 105, 7, 7),
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const HomeScreen()));
+              },
+              icon: const Icon(
+                Icons.home,
+                color: Colors.grey,
+              ),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => const Completed()));
+              },
+              icon: const Icon(Icons.check_box),
+              color: const Color.fromARGB(255, 255, 255, 255),
+            ),
+            label: 'completed',
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => const Favorites()));
+              },
+              icon: const Icon(Icons.favorite),
+              color: Colors.grey,
+            ),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
   }
 }
