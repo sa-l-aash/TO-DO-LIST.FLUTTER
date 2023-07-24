c488937ec17678b82a0965b66afae29e45d8e1dd
sa-l-aash <salaashkaelo2gmail.com>
2023-07-24 18:28:31 +0300

    added variables for table columns

--- app/lib/Globals/Globals.dart
+++ app/lib/Globals/Globals.dart
@@ -3,5 +3,14 @@ import 'package:sqflite/sqflite.dart';
 late Database todoDB;
 String userTable = 'Users';
 String userTBName = 'Name';
+String userTBEmail = 'email';
+String userTBPassword = 'password';
+
+
+String tasksTable = 'tasks';
+String tasksTBId = 'id';
+String tasksTBTitle = 'title';
+String tasksTBDescription = 'description';
+String tasksTBIsDone = 'isDone';
 
 int myloginid = 0;
