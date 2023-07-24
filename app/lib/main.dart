a15b70e57f240173b2fa80e358d8937159531d5b
sa-l-aash <salaashkaelo2gmail.com>
2023-07-24 12:31:42 +0300

    db to be created once the app opens

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,17 +1,33 @@
 //this is very important
 import 'package:app/screens/home_screen.dart';
-// import 'package:app/screens/tasks_page.dart';
 import 'package:flutter/material.dart';
-//importing home_screen
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
+import 'package:app/Globals/Globals.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 //this is the main method
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+void main() async {
+  runApp(const MyApp());
+
+  Directory documentsDirectory = await getApplicationDocumentsDirectory();
+  String pathToDB = "${documentsDirectory.path}/todo.db";
+
+  todoDB = await openDatabase(
+    pathToDB,
+    version: 2,
+    onCreate: (Database db, int version) async{
+      await db.execute(
+        'CREATE TABLE $userTable (name TEXT, email VARCHAR, password VARCHAR)',
+      );
+    },
+    onUpgrade: (Database db, int oldVersion, int newVersion)async{
+        await db.execute(
+          'CREATE TABLE tasks(id INTEGER PRIMARY KEY , title TEXT, description VARCHAR , isDone INTEGER)');
+    }
   );
   //its a function that runs everything on the target device
-  runApp(const MyApp());
 }
 
 //this is the main widget class of this application
